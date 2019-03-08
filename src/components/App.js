933bc6f2e15330d535361e18989e6e6650aed4d4
Jimmy Karlsson <jimmy.ch.karlsson.dev@gmail.com>
2019-03-08 14:37:49 +0100

    Added some mocked questions

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,49 @@
 import React, { useState } from 'react';
 
+
+const Questions = [
+  {   
+    category:"Geography", 
+    type:"multiple",
+    difficulty:"easy",
+    question:"What is the capital of Jamaica?",
+    correct_answer:"Kingston",
+    incorrect_answers:["San Juan","Port-au-Prince","Bridgetown"]    
+  },
+  {
+    category:"Geography",
+    type:"multiple",
+    difficulty:"easy",
+    question:"What name was historically used for the Turkish city currently known as Istanbul?",
+    correct_answer:"Constaninople",
+    incorrect_answers:["H&uuml;davendigar","S&ouml;\u011f&uuml;t","Adrianople"]
+  },
+  {
+    category:"Geography",
+    type:"multiple",
+    difficulty:"easy",
+    question:"How many countries does the United States share a land border with?",
+    correct_answer:"2",
+    incorrect_answers:["1","3","4"]
+  }
+]
+ 
+
+
 export function App({ initialData }) {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);  
+  const randomIndex = Math.floor(Math.random()*Questions.length);
   return (
     <div>
-      <h1>{initialData.appName}</h1>
-      This is a sample stateful and server-side rendered React application.
-      <br />
-      <br />
-      Here is a button that will track how many times you click it:
-      <br />
-      <br />
-      <button onClick={() => setCount(count + 1)}>{count}</button>
+      <div>{Questions[randomIndex].question}</div>
+      <div>
+        <div>{Questions[randomIndex].correct_answer}</div>
+        <div>{Questions[randomIndex].incorrect_answers[0]}</div>
+        <div>{Questions[randomIndex].incorrect_answers[1]}</div>
+        <div>{Questions[randomIndex].incorrect_answers[2]}</div>
+      </div>
     </div>
+      
+
   );
 }
